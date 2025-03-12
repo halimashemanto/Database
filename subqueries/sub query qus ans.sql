6b61a1fdf4bdae5ba87296bf28a9103fd2059c50
halimashemanto <suprovashemanto@gmail.com>
2025-03-12 15:34:03 +0600

    add

--- subqueries/sub query qus ans.sql
+++ subqueries/sub query qus ans.sql
@@ -51,3 +51,36 @@ where manager_id in
             from employees
             where last_name ='King');
 
+                            //new qus 12.03.25//
+
+ //57. Display details of departments in which the maximum salary
+--is more than 10000.                      
+
+Ans;
+               
+ SELECT * FROM DEPARTMENTS
+ WHERE DEPARTMENT_ID IN
+                ( SELECT DEPARTMENT_ID FROM EMPLOYEES
+                    GROUP BY DEPARTMENT_ID
+                    HAVING MAX(SALARY)>10000);
+
+//-- 58. Display details of departments managed by ‘Smith’.
+
+Ans;
+
+SELECT * FROM DEPARTMENTS
+WHERE MANAGER_ID IN
+            (SELECT EMPLOYEE_ID 
+             FROM EMPLOYEES
+             WHERE FIRST_NAME='SMITH');
+
+ //--60. Display employees who did not do any job in the past.
+--SELECT * FROM EMPLOYEES WHERE EMPLOYEE_ID
+--NOT IN (SELECT EMPLOYEE_ID FROM JOB_HISTORY)
+
+Ans;
+
+SELECT * FROM EMPLOYEES
+WHERE EMPLOYEE_ID NOT IN
+            (SELECT EMPLOYEE_ID
+                FROM JOB_HISTORY);    
