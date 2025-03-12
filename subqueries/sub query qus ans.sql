a8ee2d29be304006ea796dba5057a60e27364c5e
halimashemanto <suprovashemanto@gmail.com>
2025-03-12 16:29:43 +0600

    add

--- subqueries/sub query qus ans.sql
+++ subqueries/sub query qus ans.sql
@@ -84,3 +84,43 @@ SELECT * FROM EMPLOYEES
 WHERE EMPLOYEE_ID NOT IN
             (SELECT EMPLOYEE_ID
                 FROM JOB_HISTORY);    
+
+//-- 63. Display details of manager who manages more than 5 employees.
+
+Ans;
+
+ SELECT FIRST_NAME
+ FROM EMPLOYEES
+ WHERE EMPLOYEE_ID IN
+            (SELECT MANAGER_ID 
+            FROM EMPLOYEES
+            GROUP BY MANAGER_ID
+            HAVING COUNT(*)>5);
+
+
+//-- 65. Display the departments into which no employee joined in last two years.
+
+Ans;
+
+ SELECT  * FROM DEPARTMENTS
+ WHERE DEPARTMENT_ID NOT IN
+            ( SELECT DEPARTMENT_ID 
+             FROM EMPLOYEES
+             WHERE FLOOR((SYSDATE-HIRE_DATE)/365) < 2 );
+
+
+// 67. Display details of current job for employees who worked as IT Programmers in the past. 
+
+Ans;
+
+ SELECT * FROM JOBS
+ WHERE JOB_ID IN
+            (SELECT JOB_ID 
+            FROM EMPLOYEES
+            WHERE EMPLOYEE_ID IN
+                         (SELECT EMPLOYEE_ID
+                         FROM JOB_HISTORY 
+                         WHERE JOB_ID='IT_PROG'));
+
+
+
